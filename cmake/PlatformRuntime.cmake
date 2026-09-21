a11818d0a3677b55845830db70e2c9cf5091841e
vladelaina <vladelaina@gmail.com>
2026-09-21 19:56:19 +0800

    fix: microsoft store apps cannot be launched with administrator privileges

--- cmake/PlatformRuntime.cmake
+++ cmake/PlatformRuntime.cmake
@@ -4,6 +4,7 @@ if(WIN32)
     src/platform/windows/windows.c
     src/platform/windows/windows_directory.c
     src/platform/windows/windows_package.c
+    src/platform/windows/windows_package_shortcut.c
     src/platform/windows/windows_startup.c
     src/platform/windows/windows_autostart.cpp
     src/platform/windows/windows_autostart_config.cpp
